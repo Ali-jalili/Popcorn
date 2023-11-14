d6d06c3656f1a5b808dc06154b0d3a956c145019
Fatemeh <fatemehzalaghi@gmail.com>
2023-11-14 16:41:54 +0330

    folder name changed

---
+++ popcorn/src/App.jsx
@@ -0,0 +1,112 @@
+import { useState } from "react";
+import './App.css'
+import NavBar from './Components/NavBar'
+import NumResults from './Components/NumResults'
+import Main from "./Components/Main";
+import Box from "./Components/Box";
+import MovieList from "./Components/MovieList";
+import WatchedMoviesList from "./Components/WatchedMoviesList";
+import WatchedSummary from "./Components/WatchedSummary";
+
+// const tempMovieData = [
+//   {
+//     imdbID: "tt1375666",
+//     Title: "Inception",
+//     Year: "2010",
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//   },
+//   {
+//     imdbID: "tt0133093",
+//     Title: "The Matrix",
+//     Year: "1999",
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+//   },
+//   {
+//     imdbID: "tt6751668",
+//     Title: "Parasite",
+//     Year: "2019",
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+//   },
+// ];
+
+// const tempWatchedData = [
+//   {
+//     imdbID: "tt1375666",
+//     Title: "Inception",
+//     Year: "2010",
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//     runtime: 148,
+//     imdbRating: 8.8,
+//     userRating: 10,
+//   },
+//   {
+//     imdbID: "tt0088763",
+//     Title: "Back to the Future",
+//     Year: "1985",
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+//     runtime: 116,
+//     imdbRating: 8.5,
+//     userRating: 9,
+//   },
+// ];
+
+// const KEY = "77ecf1a8";
+
+
+export default function App() {
+  const [movies, setMovis] = useState([]);
+  const [watched] = useState([]);
+
+
+
+  /**
+   * Fetches data from the specified URL and sets the retrieved data to the "Movis" state.
+   * @param {string} url - The URL to fetch the data from.
+   * @returns None
+   */
+  fetch(`https://api.apify.com/v2/datasets/9uUoV55WrsBBh2aNa/items?clean=true&format=json`)
+    .then(res => res.json())
+    .then(data => setMovis(data.Search))
+
+
+  return (
+    <>
+      <NavBar>
+
+        <NumResults movies={movies} />
+      </NavBar>
+
+      <Main>
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </Box>
+      </Main>
+    </>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
